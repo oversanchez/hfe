2ff592812bc25c150289527c8757fa8085d5ddfe
Oliver Sanchez <oliver.sanchez.a@gmail.com>
2017-04-18 11:46:29 -0500

    modifico : seccion

---
+++ database/migrations/2017_04_18_161753_creacion_tabla_seccion.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreacionTablaSeccion extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seccion', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->integer('numero');
+            $table->integer('nro_vacantes');
+            $table->boolean('activo');
+            $table->enum('turno',['Mañana','Tarde','Nocturna']);
+            $table->enum('tipo_calificacion',['Literal','Vigesimal']);
+
+            $table->integer('anio_lectivo_id')->unsigned();
+            $table->foreign('anio_lectivo_id')->references('id')->on('anio_lectivo');
+
+            $table->integer('grado_id')->unsigned();
+            $table->foreign('grado_id')->references('id')->on('grado');
+
+            $table->integer('trabajador_id')->unsigned()->nullable();
+            $table->foreign('trabajador_id')->references('id')->on('trabajador');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seccion');
+    }
+}
