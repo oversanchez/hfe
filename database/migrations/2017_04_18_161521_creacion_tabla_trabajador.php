d666c5d2e1dc71875a82f533b8498c16c6a02ac2
Oliver Sanchez <oliver.sanchez.a@gmail.com>
2017-04-18 11:37:50 -0500

    se agrego : categoria_trabajador,trabajador modifico : seccion

---
+++ database/migrations/2017_04_18_161521_creacion_tabla_trabajador.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreacionTablaTrabajador extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trabajador', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('activo');
+
+            $table->integer('persona_id')->unsigned();
+            $table->foreign('persona_id')->references('id')->on('persona');
+
+            $table->integer('categoria_trabajador_id')->unsigned();
+            $table->foreign('categoria_trabajador_id')->references('id')->on('categoria_trabajador');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trabajador');
+    }
+}
